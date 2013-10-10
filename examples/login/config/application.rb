440decd9340aae0b8841ce43cc1fbb22dfecf3f3
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-10-10 11:30:50 +0300

    Add login demo

---
+++ examples/login/config/application.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+module Solarized
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    config.web_console.style.colors = :solarized_light
+    config.web_console.command = 'login'
+
+    config.web_console.timeout = 45 if ENV['LONG_POLLING']
+    config.allow_concurrency = true if ENV['LONG_POLLING']
+  end
+end
